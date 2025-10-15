ab1eab62301b2a4bdd87d2b82115e9ea930c0f48
X43K <111780017+X43K@users.noreply.github.com>
2025-10-15 14:20:11 +0200

    Update start-latest-serv.sh

--- start-latest-serv.sh
+++ start-latest-serv.sh
@@ -4,7 +4,7 @@
 user=$(whoami)
 
 # Buscar el archivo con la versión más alta
-script=$(ls /home/"$user"/ServTableIP/serv-table-ip*.py 2>/dev/null | \
+script=$(ls /home/"$user"/ServTableIP/ServTableIP*.py 2>/dev/null | \
     sed -E 's/.*serv-table-ip-([0-9]+)\.([0-9]+)\.py/\1 \2 &/' | \
     sort -k1,1n -k2,2n | \
     tail -n 1 | \
@@ -12,7 +12,7 @@ script=$(ls /home/"$user"/ServTableIP/serv-table-ip*.py 2>/dev/null | \
 
 # Verificar que se haya encontrado un archivo válido
 if [ -z "$script" ]; then
-    echo "No se encontró ningún archivo válido en /home/$user/serv-table-ip/"
+    echo "No se encontró ningún archivo válido en /home/$user/ServTableIP/"
     exit 1
 fi
 
